23146effc003b986bd5608e38229e0e31ac9cd22
Willian Gonçalves <willian.p.g@outlook.com>
2024-01-19 16:44:06 -0300

    Update na seção

---
+++ 04-ciencia-da-computacao/secao-01-introducao-a-python/dia-01-aprendendo-python/exercicio-01/README.md
@@ -0,0 +1,3 @@
+# Exercício 1
+
+No terminal, inicialize duas variáveis a e b, sendo a = 10 e b = 5. Mostre o resultado das 7 operações básicas (soma, subtração, multiplicação, divisão, divisão inteira, potenciação e módulo) envolvendo essas variáveis.
---
+++ 04-ciencia-da-computacao/secao-01-introducao-a-python/dia-01-aprendendo-python/exercicio-01/exercicio-01.py
@@ -0,0 +1,12 @@
+a = 10
+b = 5
+
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+divisao_inteira = a // b
+potenciacao = a ** b
+modulo = a % b
+
+print(soma, subtracao, multiplicacao, divisao, divisao_inteira, potenciacao, modulo)
