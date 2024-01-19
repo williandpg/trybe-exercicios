448997175e2f68bd4199b812101f61992e1683fa
Willian Gonçalves <willian.p.g@outlook.com>
2024-01-19 16:54:36 -0300

    Update na seção

---
+++ 04-ciencia-da-computacao/secao-01-introducao-a-python/dia-01-aprendendo-python/exercicio-03/exercicio-03.py
@@ -0,0 +1,5 @@
+teste =  'colocando um ponro e virgula no final da linha';
+
+print(teste);
+
+#Assim como em JavaScript, Python permite que você coloque um ponto e vírgula no final de uma instrução, mas não é necessário.
