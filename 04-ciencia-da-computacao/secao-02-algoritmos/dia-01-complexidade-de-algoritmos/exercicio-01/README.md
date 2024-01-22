70bd94a3daa361493edabdac6fa8c7b09dd84c61
Willian Gonçalves <willian.p.g@outlook.com>
2024-01-22 14:51:19 -0300

    Update na seção

---
+++ 04-ciencia-da-computacao/secao-02-algoritmos/dia-01-complexidade-de-algoritmos/exercicio-01/README.md
@@ -0,0 +1,12 @@
+# Exercício 1
+
+Qual é a Ordem de Complexidade (complexidade de tempo) do algoritmo abaixo? E a complexidade de espaço?
+
+```sh
+def multiply_array(numbers):
+    result = 1
+    for number in numbers:
+        result *= number
+
+    return result
+```
---
+++ 04-ciencia-da-computacao/secao-02-algoritmos/dia-01-complexidade-de-algoritmos/exercicio-01/exercicio-01.py
@@ -0,0 +1,7 @@
+# Complexidade de Tempo: O(n)
+
+# Complexidade de Espaço: O(1)
+
+# Sobre a complexidade de tempo: Independentemente do tamanho da entrada (numbers) o nosso laço for irá percorrer cada elemento do array de tamanho n. Por isso, sua complexidade de tempo é O(n).
+
+# Sobre a complexidade espacial: Vamos operar sobre um número constante de variáveis que, por sua vez, armazenam um único elemento em memória. A variável result será responsável por armazenar o resultado numérico de nossa operação de multiplicação. Por isso, nossa complexidade espacial é O(1).
